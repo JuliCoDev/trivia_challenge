6045852526b306fcf9de7e486a55e1fc8dbd048e
JulianaGarciaCo <julianageraldineco@gmail.com>
2022-09-22 12:44:05 -0500

    Add page home whit styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,15 @@
+import styles from './css/home.module.css';
+
 const Home = () =>{
+    console.log(styles.container);
     return(
-        <h1>Bienvenido</h1>
+        <div className={styles.container}>
+            <h1>Welcome to trivia challenge</h1>
+            <p>Can you sore 100%?</p>
+            <button>
+                Start!
+            </button>
+        </div>        
     )
 }
 
