884f51591558df1b78b73ac700861a863ef79c8a
JulianaGarciaCo <julianageraldineco@gmail.com>
2022-09-26 18:31:16 -0500

    Add styles to Home
    1. Modify background.
    2. Add new font
    3. Change styles of title, logo and botton in page homne

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ Trivia challenge is a game in wich you can put to the test your knowlege in dife
 * ✔  **Multiple choice:** You can select one options of the possible answers.
 * ✅ **Feedback:** You get information about your correct and incorrect answers and the socore obtained.
 
+## Installation
+
+React  is available from npm. You can install it (and its required dependencies)
+using:
+
+```sh
+npm install
+```
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import styles from './css/home.module.css';
-
+import tiviaLogo from '../../assets/trivia_logo.png';
 const Home = () =>{
     console.log(styles.container);
     return(
         <div className={styles.container}>
             <h1>Welcome to trivia challenge</h1>
-            <p>Can you sore 100%?</p>
+            <img src={tiviaLogo}/>
             <button>
                 <a href='/questions'>Start!</a>
             </button>
